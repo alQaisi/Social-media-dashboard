c01b96631fc853534351a09aedf96c7afacf2244
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-07-02 13:56:27 +0300

    using localstorage to save theme

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-var theme=document.querySelector("#theme");
-theme.addEventListener('click',(evt)=>{
-    document.body.classList.toggle('dark');
-});
+var themeSelector=document.querySelector("#theme");
+themeSelector.addEventListener("click",selectTheme);
+
+function selectTheme(){
+    const theme=document.body.className=="dark"?"":"dark";
+    localStorage.setItem("theme",theme);
+    document.body.className=theme;
+}
+
+function loadTheme(){
+    const theme=localStorage.getItem("theme");
+    themeSelector.checked=theme=="dark"?true:false;
+    document.body.className=theme;
+}
+loadTheme();
